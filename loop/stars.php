28466c69e7ae9bd763130009303a847520472db5
Yuz <wwowkdrr@gmail.com>
2025-05-02 11:43:29 +0800

    畫星星迴圈練習/菱形

--- loop/stars.php
+++ loop/stars.php
@@ -36,12 +36,11 @@ for($i=0;$i<5;$i++){
             echo "★";
         }
     }
-
     echo "<br>";
 }
 ?>
 
-<h3>倒三角形 - 寫法2</h3>
+<h3>倒三角形 - 寫法 2</h3>
 <?php
 for($i=4;$i>=0;$i--){
     for($j=0;$j<5;$j++){
@@ -49,11 +48,81 @@ for($i=4;$i>=0;$i--){
             echo "★";
         }
     }
-
     echo "<br>";
 }
 ?>
 
+<h3>正三角形</h3>
+<style>
+     *{
+        font-family:'Courier New', Courier , monospace;
+     }
+     /* 所有的標籤 */
+     /* 解決字型問題無法印出漂亮的三角形 */
+</style>
+<?php
+for($i=0;$i<5;$i++){
+
+   //星星前要印空白把三角形推出來
+    for($k=0;$k<5-1-$i;$k++){//空白最大值5-1再扣外圈的i值
+        echo "&nbsp;"; 
+    }
+    for($j=0; $j<$i * 2+1; $j++){
+        echo "*";
+    } 
+    echo "<br>";
+}
+
+?>
+
+<h3>變數控制的三角形</h3>
+
+<?php
+$stars=8;
+for($i=0;$i<$stars;$i++){
+
+   //星星前要印空白把三角形推出來
+    for($k=0;$k<$stars-1-$i;$k++){//空白最大值5-1再扣外圈的i值
+        echo "&nbsp;"; 
+    }
+    for($j=0; $j<$i * 2+1; $j++){
+        echo "*";
+    } 
+    echo "<br>";
+}
+
+?>
+
+<h3>菱形</h3>
+<?php
+$stars=12; 
+//如果可以讓人輸入任意數字，要考慮呈現時的問題
+// 但實際上只有奇數跑出來才正常
+
+if($stars%2==0){//解法:強制變奇數
+    $stars=$stars+1;
+}
+
+for($i=0;$i<$stars;$i++){
+
+    if($i<=floor($stars/2)){
+        $y=$i;
+
+    }else{
+        $y=$stars-1-$i;
+    }
+
+    //畫空白
+    for($j=0;$j<floor($stars/2)-$y;$j++){
+        echo "&nbsp";
+    }
+    //畫星星
+    for($k=0;$k<$y*2+1;$k++){
+        echo "*";
+    }
+    echo "<br>";
+}
+?>
 
 </body>
 </html>
