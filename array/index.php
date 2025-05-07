7d47eae3c1ef92c48be6793ddc83378aeb14a27b
Yuz <wwowkdrr@gmail.com>
2025-05-07 15:55:53 +0800

    用陣列反轉元素

--- array/index.php
+++ array/index.php
@@ -299,7 +299,30 @@ $yy=2126;
 echo $d[(2126)%60-4];
 ?>
 
+<h2>請設計一支程式，在不產生新陣列的狀況下，將一個陣列的元素順序反轉(利用迴圈)</h2>
+例：$a=[2,4,6,1,8] 反轉後 $a=[8,1,6,4,2]
 
+<?php
+$a=[2,4,6,1,8];
+echo "<pre>";
+print_r($a);
+echo "</pre>";
+
+for($i=0; $i<floor(count($a)/2); $i++){
+    $temp=$a[$i];
+    $a[$i]=$a[count($a)-1-$i];
+    $a[count($a)-1-$i]=$temp;
+}
+echo "<pre>";
+print_r($a);
+echo "</pre>";
+
+echo "<pre>";
+print_r(array_reverse($a));
+echo "</pre>";
+
+
+?>
 </body>
 </html>
 
