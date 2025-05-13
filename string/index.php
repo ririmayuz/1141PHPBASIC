517132bf753355201032b1515839084bdf6f28c3
Yuz <wwowkdrr@gmail.com>
2025-05-13 10:23:29 +0800

    子字串取用練習

--- string/index.php
+++ string/index.php
@@ -33,7 +33,7 @@
 
     //$str=str_replace(["a","d","w","1","2","3"],"*",$str);
     //$str=str_replace(str_split($str,2),"*",$str);
-    $str = str_repeat("*", strlen($str)); //英文輸入用strlen就可以/mb_strlen?
+    $str = str_repeat("*", strlen($str)); //英文輸入用strlen就可以/mb_strlen?mb=支援多語系
 
 
     echo $str;
@@ -65,16 +65,30 @@
     //經過上個練習，這時候的 $str 已經是一個陣列了
     //join( glue , array )
     //join(string $separator, array $array): string
-    $str=join(" ",$str);
+    $str = join(" ", $str);
     echo $str;
 
     ?>
+
     <h2>子字串取用</h2>
     <li>將” The reason why a great man is great is that he resolves to be a great man”只取前十字成為” The reason…””</li>
     <p></p>
 
     <?php
+    $str = "The reason why a great man is great is that he resolves to be a great man";
+    $str=mb_substr($str,0,10,"utf-8") . "...";
+    echo $str;
+    
+    ?>
 
+    <h2>尋找字串與HTML、css整合應用</h2>
+    <ul>
+        <li> 給定一個句子，將指定的關鍵字放大</li>
+        <li> “學會PHP網頁程式設計，薪水會加倍，工作會好找”F</li>
+        <li> 請將上句中的 “程式設計” 放大字型或變色.</li>
+    </ul>
+
+    <?php
 
     ?>
 </body>
