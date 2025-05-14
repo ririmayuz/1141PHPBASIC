f8ae0296a73134c24226ddeea3b4d4e17966f96d
Yuz <wwowkdrr@gmail.com>
2025-05-14 14:27:31 +0800

    使用div繪製日曆

--- datetime/calendar.php
+++ datetime/calendar.php
@@ -6,86 +6,167 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>線上月曆</title>
     <style>
-    h1 {
-        text-align: center;
-        color: blue;
-    }
+        h1 {
+            text-align: center;
+            color: blue;
+        }
 
-    table {
-        width: 60%;
-        border-collapse: collapse;
-        margin: 0 auto; /* 只有在設定寬度後才有效，讓表格置中 */
-    }
+        table {
+            width: 60%;
+            border-collapse: collapse;
+            margin: 0 auto;
+            /* 只有在設定寬度後才有效，讓表格置中 */
+        }
 
-    td {
-        border: 1px solid black;
-        text-align: center;
-        padding: 5px 10px; /* 垂直5px、水平10px 的內距 */
-    }
+        td {
+            border: 1px solid black;
+            text-align: center;
+            padding: 5px 10px;
+            /* 垂直5px、水平10px 的內距 */
+        }
 
-    /* 今日的樣式 */
-    .today {
-        background-color: lightblue;
-        font-weight: bold;
-        color: white;
-    }
+        /* 今日的樣式 */
+        .today {
+            background-color: lightblue;
+            font-weight: bold;
+            color: white;
+        }
 
-    /* 其他月份的日期樣式 */
-    .other-month {
-        background-color: gray;
-        color: #aaa; /* 淡色文字 */
-    }
+        /* 其他月份的日期樣式 */
+        .other-month {
+            background-color: gray;
+            color: #aaa;
+            /* 淡色文字 */
+        }
 
-    /* 週末（假日）樣式 */
-    .holiday {
-        background-color: pink;
-        color: white;
-    }
+        /* 週末（假日）樣式 */
+        .holiday {
+            background-color: pink;
+            color: white;
+        }
 
-    /* 滑鼠移到格子上時的效果（不套在第一列） */
-    tr:not(tr:nth-child(1)) td:hover {
-        background-color: lightseagreen;
-        cursor: pointer;
-        font-size: 16px;
-        font-weight: bold;
-    }
+        /* 滑鼠移到格子上時的效果（不套在第一列） */
+        tr:not(tr:nth-child(1)) td:hover {
+            background-color: lightseagreen;
+            cursor: pointer;
+            font-size: 16px;
+            font-weight: bold;
+        }
 
-    /* 過去的日期樣式（可搭配 today / holiday / other-month 同時出現） */
-    .pass-date {
-        /* background-color: lightgray; */ /* 如果你想淡化背景可以打開 */
-        color: #aaa; /* 文字變淡 */
-    }
+        /* 過去的日期樣式（可搭配 today / holiday / other-month 同時出現） */
+        .pass-date {
+            /* background-color: lightgray; */
+            /* 如果你想淡化背景可以打開 */
+            color: #aaa;
+            /* 文字變淡 */
+        }
 
-    .date-num{
-        font-size: 14px;
-        text-align: left;
-    }
+        .date-num {
+            font-size: 14px;
+            text-align: left;
+        }
 
-    .date-event{
-        height: 40px;
-    }
-</style>
+        .date-event {
+            height: 40px;
+            width: 80px;
+            text-align: center;
+            line-height: 40px;
+
+        }
+
+        .box,
+        .th-box {
+            width: 50px;
+            height: 50px;
+            background-color: lightblue;
+            display: inline-block;
+            border: 1px solid blue;
+            box-sizing: border-box;
+            margin-left: -1px;
+            margin-top: -1px;
+        }
+
+        .box-container {
+            width: 350px;
+            margin: 0 auto;
+            box-sizing: border-box;
+            padding-left: 1px;
+            padding-top: 1px;
+        }
+
+        .th-box {
+            height: 25px;
+            text-align: center;
+        }
+    </style>
 
 </head>
 
 <body>
+
+    <!-- <div class="box-container">
+<?php
+
+/* for($i=0;$i<20;$i++){
+    echo "<div class='box'>";
+        echo $i;
+    echo "</div>";
+} */
+?>
+</div> -->
+
+
     <h1>線上日曆</h1>
 
     <?php
-    $today = date("Y-m-d");
-    $firstDay = date("Y-m-01"); //文字模板  
+    $month = 5;
+    $today = date("Y-$month-d");
+    $firstDay = date("Y-$month-01"); //文字模板  
     $firstDayWeek = date("w", strtotime($firstDay)); // w = 在這一週的哪一天
     $theDaysOfMonth = date("t", strtotime($firstDay));
     //變數用駝峰式命名法:小駝峰 theDaysOfMonth/大駝峰 TheDaysOfMonth
     //蛇型/鍊式命名法 the_days_of_month / the-days-of-month
-    $spDate=[
-        '2025-05-01'=>'勞動節',
-        '2025-05-11'=>'母親節',
-        '2025-05-30'=>'端午節'
+
+    $spDate = [
+        '2025-05-01' => '勞動節',
+        '2025-05-11' => '母親節',
+        '2025-05-30' => '端午節'
     ];
+
+    $monthDays = [];
+
+    //填入空白日期
+    for ($i = 0; $i < $firstDayWeek; $i++) {
+        $monthDays[] = "&nbsp;";
+    }
+    //填入當日日期
+    for ($i = 0; $i < $theDaysOfMonth; $i++) {
+        $timestamp = strtotime(" $i days", strtotime($firstDay));
+        $date = date("d", $timestamp);
+        $monthDays[] = $date;
+    }
+    //建立外框及標題
+    echo "<div class='box-container'>";
+
+    echo "<div class='th-box'>日</div>";
+    echo "<div class='th-box'>一</div>";
+    echo "<div class='th-box'>二</div>";
+    echo "<div class='th-box'>三</div>";
+    echo "<div class='th-box'>四</div>";
+    echo "<div class='th-box'>五</div>";
+    echo "<div class='th-box'>六</div>";
+
+    //使用foreach迴圈印出日期
+    foreach ($monthDays as $day) {
+
+        echo "<div class='box'>";
+        echo $day;
+        echo "</div>";
+    }
+    echo "</div>";
     ?>
 
-    <h2 style='text-align:center;'><?=date("Y 年 m 月"); ?></h2>
+    <h2 style='text-align:center;'><?= date("Y 年 m 月"); ?></h2>
     <table>
         <tr>
             <td>一</td>
@@ -96,62 +177,62 @@
             <td>六</td>
             <td>日</td>
         </tr>
-    
 
-<?php
-for ($i = 0; $i < 6; $i++) {
-    echo "<tr>";
 
-    for ($j = 0; $j < 7; $j++) {
-        // === 計算當前格子的日期 ===
-        // 原寫法是直接算日數：$j + 1 + ($i * 7) - $firstDayWeek
-        // 改用 strtotime 處理跨月更準確，不用額外判斷
-        $day = $j + ($i * 7) - $firstDayWeek;
-        $timestamp = strtotime("$day days", strtotime($firstDay));
-        $date = date("Y-m-d", $timestamp);
-        $class = ""; // 初始化格子 class
+        <?php
+        for ($i = 0; $i < 6; $i++) {
+            echo "<tr>";
 
-        // === 加入對應的 class ===
+            for ($j = 0; $j < 7; $j++) {
+                // === 計算當前格子的日期 ===
+                // 原寫法是直接算日數：$j + 1 + ($i * 7) - $firstDayWeek
+                // 改用 strtotime 處理跨月更準確，不用額外判斷
+                $day = $j + ($i * 7) - $firstDayWeek;
+                $timestamp = strtotime("$day days", strtotime($firstDay));
+                $date = date("Y-m-d", $timestamp);
+                $class = ""; // 初始化格子 class
 
-        // 週末（六、日）
-        if (date("N", $timestamp) > 5) {
-            $class .= " holiday";
-        }
+                // === 加入對應的 class ===
 
-        // 今日
-        if ($today == $date) {
-            $class .= " today";
+                // 週末（六、日）
+                if (date("N", $timestamp) > 5) {
+                    $class .= " holiday";
+                }
 
-        // 非本月日期
-        } else if (date("m", $timestamp) != date("m", strtotime($firstDay))) {
-            $class .= " other-month";
-        }
+                // 今日
+                if ($today == $date) {
+                    $class .= " today";
 
-        // 已過的日期（今天之前）
-        if ($timestamp < strtotime($today)) {
-            $class .= " pass-date";
-        }
+                    // 非本月日期
+                } else if (date("m", $timestamp) != date("m", strtotime($firstDay))) {
+                    $class .= " other-month";
+                }
 
-        // === 輸出表格格子（只顯示日） ===
-        echo "<td class='$class' data-date='$date' title='這是 $date'>";
-            
-            echo "<div class='date-num'>";
-            echo date("d", $timestamp); 
-            echo "</div>";
+                // 已過的日期（今天之前）
+                if ($timestamp < strtotime($today)) {
+                    $class .= " pass-date";
+                }
 
-            echo"<div class='date-event'>";
-                if(isset($spDate[$date])){
+                // === 輸出表格格子（只顯示日） ===
+                echo "<td class='$class' data-date='$date' title='這是 $date'>";
+
+                echo "<div class='date-num'>";
+                echo date("d", $timestamp);
+                echo "</div>";
+
+                echo "<div class='date-event'>";
+                if (isset($spDate[$date])) {
                     echo $spDate[$date];
                 }
 
-            echo "</div>";
+                echo "</div>";
 
-        echo "</td>";
-    }
+                echo "</td>";
+            }
 
-    echo "</tr>";
-}
-?>
+            echo "</tr>";
+        }
+        ?>
 
     </table>
 </body>
