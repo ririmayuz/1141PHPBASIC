517cbcb83e9b6b00224ee74d2966ac2546a3ebf4
Yuz <wwowkdrr@gmail.com>
2025-05-14 10:40:29 +0800

    將當月以外的日期/今日加上顏色區分

--- datetime/calendar.php
+++ datetime/calendar.php
@@ -23,6 +23,20 @@
             text-align: center;
             padding: 5px 10px;
         }
+
+        .today{
+            background-color: lightblue;
+            font-weight: bold;
+        }
+
+        .other-month{
+            background-color: lightblue;
+            color: #aaa;
+        }
+        .holiday{
+            background-color: pink;
+            color: white;
+        }
     </style>
 </head>
 
@@ -37,7 +51,7 @@
     //變數用駝峰式命名法:小駝峰 theDaysOfMonth/大駝峰 TheDaysOfMonth
     //蛇型/鍊式命名法 the_days_of_month / the-days-of-month
     ?>
-
+    <h2><?php date("Y / m") ?></h2>
     <table>
         <tr>
             <td>一</td>
@@ -57,15 +71,22 @@
         for ($j = 0; $j < 7; $j++) {
             //畫格子之前先算"這是哪一天?" 
             $day=$j+($i*7)-$firstDayWeek;
-            $date = date("Y-m-d",strtotime("$day days",strtotime($firstDay)));
+            $timestamp = strtotime("$day days",strtotime($firstDay));
+            $date = date("Y-m-d",$timestamp);
             //$day=$j+1+($i*7)-$firstDayWeek; 用strtotime就不用+1了
             //strtotime("+1");
             //$d=date("Y-m-$day"); <-這三行是原本的程式碼
-
-            echo "<td>";            
+            if($today==$date){
+                echo "<td class='today'>";
+            }else if(date("m",$timestamp) != date("m",strtotime($firstDay))){
+                echo "<td class= 'other-month'>";
+            }else{
+                echo "<td>";
+            }
+                        
             //if($day>0 && $day<=$theDaysOfMonth){
                 echo $date;
-            //} 有strtotime也不需要判斷了
+            //} 改用strtotime，已經不需要判斷
             // echo $j + 1 + ($i * 7) - $firstDayWeek; <-
             //if的簡寫法:沒有大括號甚至不用else?
             
